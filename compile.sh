592791c97bbc8b22345e42010b9b755f00c05faa
IURY TERCIO SIMOES-SOUSA <simoesiury@gmail.com>
2021-01-25 19:45:15 -0500

    mkdir for run and build folders

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
+mkdir -p build run_N0 run_NC run_NZ
 rm build/* & cd build
 ../../../tools/genmake2 -mods=../code -mpi -of=../../../tools/build_options/linux_amd64_gfortran
 make depend
 make
-cp mitgcmuv ../run/.
-cd ../run/
+cp mitgcmuv ../run/. & cd ../run/
